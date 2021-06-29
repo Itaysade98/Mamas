dedfe410faf50bc5638d5ec06e647186a8674837
itaysade98 <itaysade98@gmail.com>
2021-06-29 20:32:08 +0300

    added menu class

---
+++ Mamas/Menu.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mamas
+{
+    public static class Menu
+    {
+        private static string optionSelection;
+
+        /// <summary>
+        /// Writes and manages the applications main menu.
+        /// reads user input and calls function accordingly.
+        /// </summary>
+        public static void MainMenu()
+        {
+            do
+                {
+                Console.WriteLine("Welcome to Ichilov Hospital!");
+                Console.WriteLine("1. Clock in/out by ID number");
+                Console.WriteLine("2. Get employee current monthly salary by ID number");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Please choose an action: ");
+                optionSelection = Console.ReadLine();
+                switch (optionSelection)
+                {
+                    case "1":
+                        {
+                            ClockEmployee();
+                            Console.WriteLine("Press any key to return to menu");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            GetEmployeePaycheck();
+                            Console.WriteLine("Press any key to return to menu");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+
+                    default:
+                        Console.Clear();
+                        break;
+                }
+
+            } while (optionSelection != "3");
+         }
+
+
+        /// <summary>
+        /// asks user for employee ID and validates the input.
+        /// if the input is not a valid ID it will call itself recursively
+        /// until a valid id is given.
+        /// </summary>
+        /// <returns>valid employee ID entered by user</returns>
+        private static int GetEmployeeID()
+        {
+            Console.WriteLine("Please enter employee ID:");
+            string inputid = Console.ReadLine();
+            int id;
+            bool isnum = int.TryParse(inputid, out id);
+            if (!isnum)
+            {
+                Console.Clear();
+                Console.WriteLine("you did not input a valid id number, please try again.");
+                return GetEmployeeID();
+            }
+            else
+            {
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// handles option 1 of main menu.
+        /// gets employee by id entered by the user,
+        /// and calls its Clock() function.
+        /// prints current employee clock status.
+        /// </summary>
+        private static void ClockEmployee()
+        {
+            Employee employee = GetEmployee();
+            employee.Clock();
+            string status = "out";
+            if (employee.clockedin == 1)
+            {
+                status = "in";
+            }
+            Console.WriteLine($"{employee.name} is clocked {status}");
+        }
+
+        /// <summary>
+        /// handles option 2 of main menu.
+        /// gets employee by id entered by the user,
+        /// and calls its CalculatePaycheck() function.
+        /// prints current employees paycheck.
+        /// </summary>
+        private static void GetEmployeePaycheck()
+        {
+            Employee employee = GetEmployee();
+            float currentpay = employee.CalculatePaycheck();
+            Console.WriteLine($"{employee.name}s' paycheck for month-{employee.lasttimestamp.Month}/{employee.lasttimestamp.Year} is: {currentpay}");
+        }
+
+        /// <summary>
+        /// calls GetEmployeeID() to get valid ID
+        /// from user and checks if an employee with that id exists.
+        /// if so, it returns the employee.
+        /// if not, it calls itself again recursively until a valid employee ID is given.
+        /// </summary>
+        /// <returns>Employee with ID number entered by user</returns>
+        private static Employee GetEmployee()
+        {
+            int id = GetEmployeeID();
+            List<Employee> filteredEmployees = Utils.Employees.Where(a => a.id == id).ToList();
+            if (filteredEmployees.Count > 0)
+            {
+                return filteredEmployees[0];
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("The Employee ID entered does not exist. please try again.");
+                return GetEmployee();
+            }
+        }
+
+    }
+}
--- Mamas/Program.cs
+++ Mamas/Program.cs
@@ -12,8 +12,8 @@ namespace Mamas
     {
         /// <summary>
         /// My main function calls utils.load() to load all configuration and
-        /// employee data, then enters a while loop that functions as the menu for
-        /// the application.
+        /// employee data, then calls Menu.MainMenu() to enter a while loop that 
+        /// functions as the menu for the application.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -22,86 +22,7 @@ namespace Mamas
             {
                 Utils.Load();
                 string option;
-                do
-                {
-                    Console.WriteLine("Welcome to Ichilov Hospital!");
-                    Console.WriteLine("1. Clock in/out by ID number");
-                    Console.WriteLine("2. Get employee current monthly salary by ID number");
-                    Console.WriteLine("3. Exit");
-                    Console.WriteLine("Please choose an action: ");
-                    option = Console.ReadLine();
-                    switch (option)
-                    {
-                        case "1":
-                            {
-                                Console.WriteLine("Please enter employee ID to clock in/out: ");
-                                string inputid = Console.ReadLine();
-                                int id;
-                                bool isnum = int.TryParse(inputid, out id);
-                                if (!isnum)
-                                {
-                                    Console.WriteLine("you did not input a valid id number, please try again.");
-                                }
-                                else
-                                {
-                                    List<Employee> filteredEmployees = Utils.Employees.Where(a => a.id == id).ToList();
-                                    if (filteredEmployees.Count > 0)
-                                    {
-                                        filteredEmployees[0].Clock();
-                                        string status = "out";
-                                        if (filteredEmployees[0].clockedin == 1)
-                                        {
-                                            status = "in";
-                                        }
-
-                                        Console.WriteLine($"{filteredEmployees[0].name} is clocked {status}");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("The Employee ID entered does not exist. please try again.");
-                                    }
-                                }
-                                Console.WriteLine("Press any key to return to menu");
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            }
-
-                        case "2":
-                            {
-                                Console.WriteLine("Please enter employee ID to calculate paycheck: ");
-                                string inputid = Console.ReadLine();
-                                int id;
-                                bool isnum = int.TryParse(inputid, out id);
-                                if (!isnum)
-                                {
-                                    Console.WriteLine("you did not input a valid id number, please try again.");
-                                }
-                                else
-                                {
-                                    List<Employee> filteredEmployees = Utils.Employees.Where(a => a.id == id).ToList();
-                                    if (filteredEmployees.Count > 0)
-                                    {
-                                        float currentpay = filteredEmployees[0].CalculatePaycheck();
-                                        Console.WriteLine($"{filteredEmployees[0].name}s' paycheck for month-{filteredEmployees[0].lasttimestamp.Month}/{filteredEmployees[0].lasttimestamp.Year} is: {currentpay}");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("The Employee ID entered does not exist. please try again.");
-                                    }
-                                }
-                                Console.WriteLine("Press any key to return to menu");
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            }
-
-                        default:
-
-                            break;
-                    }
-
-                } while (option != "3");
+                Menu.MainMenu();
             }
             catch (Exception e)
             {
